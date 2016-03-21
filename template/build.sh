2214d7d0e1ac75ada6242814c6868359845b68f0
Cameron Taggart <cameron@tachyus.com>
2016-03-20 21:10:52 -0700

    See if docker run works!

--- template/build.sh
+++ template/build.sh
@@ -4,3 +4,4 @@ dotnet restore --configfile $PWD/NuGet.Config -v Minimal
 ln -sf /root/.nuget/packages/Microsoft.FSharp.Core.netcore/1.0.0-alpha-160318/runtimes/any/native/FSharp.Core.optdata /root/.nuget/packages/Microsoft.FSharp.Core.netcore/1.0.0-alpha-160318/lib/DNXCore50/FSharp.Core.optdata
 ln -sf /root/.nuget/packages/Microsoft.FSharp.Core.netcore/1.0.0-alpha-160318/runtimes/any/native/FSharp.Core.sigdata /root/.nuget/packages/Microsoft.FSharp.Core.netcore/1.0.0-alpha-160318/lib/DNXCore50/FSharp.Core.sigdata
 dotnet -v build
+dotnet run It Works!
